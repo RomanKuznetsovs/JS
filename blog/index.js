41bcc5d8ddde5f736a2e571e5d67c30f615ecab4
Roman Kuznetsov <roman.kuznetsov1@yandex.ru>
2022-08-27 22:00:51 +0300

    CRUD functional addition

--- blog/index.js
+++ blog/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 
 import mongoose from 'mongoose';
 
-import {registerValidation} from './validations/auth.js';
+import {loginValidation, postCreateValidation, registerValidation} from './validations.js';
 
 import checkAuth from './utils/checkAuth.js';
 
 import * as UserController from './controlers/UserController.js';
+import * as PostController from './controlers/PostController.js';
 
 mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.vnrtjli.mongodb.net/blog?retryWrites=true&w=majority')
 .then(() => console.log('DB OK'))
@@ -16,11 +17,15 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/login', UserController.login);
-
+app.post('/auth/login',loginValidation, UserController.login);
 app.get('/auth/me',checkAuth, UserController.getMe);
-
 app.post('/auth/register', registerValidation, UserController.register);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts',checkAuth, postCreateValidation, PostController.create);
+app.delete('/posts/:id',checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, PostController.update);
 
 app.listen(4444, (err) => {
     if (err) {
